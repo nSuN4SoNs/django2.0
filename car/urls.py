a31d7cd8825c14e8602f7c183685344e31960fc3
Obi <burgario@ukr.net>
2021-07-14 19:29:03 +0300

    urls_fix_1

--- car/urls.py
+++ car/urls.py
@@ -1,20 +1,12 @@
 from django.urls import path, include
-from car.views import CarListView, CarDetailView, CarCreateView
+from car.views import CarListView, CarDetailView, CarCreateView, CarModelListView, CarModelDetailView, CarModelCreateView, CompanyListView, CompanyDetailView, CompanyCreateView
 urlpatterns = [
     path("car/", CarListView.as_view(), name="car-list" ),
     path("car/create", CarCreateView.as_view(), name="car-create" ),
     path("car/<int:pk>", CarDetailView.as_view(), name="car-detail" ),
-]
-
-from car.views import CarModelListView, CarModelDetailView, CarModelCreateView
-urlpatterns = [
     path("carmodel/", CarModelListView.as_view(), name="carmodel-list" ),
     path("carmodel/create", CarModelCreateView.as_view(), name="carmodel-create" ),
     path("carmodel/<int:pk>", CarModelDetailView.as_view(), name="carmodel-detail" ),
-]
-
-from car.views import CompanyListView, CompanyDetailView, CompanyCreateView
-urlpatterns = [
     path("carcompany/", CompanyListView.as_view(), name="carcompany-list" ),
     path("carcompany/create", CompanyCreateView.as_view(), name="carcompany-create" ),
     path("carcompany/<int:pk>", CompanyDetailView.as_view(), name="carcompany-detail" ),
